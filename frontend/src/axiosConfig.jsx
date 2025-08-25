bbf464ab423605b04d027170deabd529d2fd483f
Hyojeong Chin <chj9127@gmail.com>
2025-08-25 16:34:07 +1000

    test

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //baseURL: '',//process.env.REACT_APP_API_BASE || 'http://localhost:5001', // local
-  baseURL: 'http://13.239.64.218:5001', // live
+  baseURL: '',//process.env.REACT_APP_API_BASE || 'http://localhost:5001', // local
+  //baseURL: 'http://13.239.64.218:5001', // live
   headers: { 'Content-Type': 'application/json' },
   // withCredentials: true,
 });
